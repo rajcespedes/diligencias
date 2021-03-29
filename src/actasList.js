6e113c0391829f066d63f94282c3b66168e5b3fe
Raudo José Céspedes <raj.cespedes@gmail.com>
2021-03-28 21:57:03 -0400

    Starting to handle data from DB

---
+++ src/actasList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function actasList(props) {
+
+    return(
+        <div>
+            <div className="row">
+                
+            </div>
+        </div>
+    );
+
+}
+
+
+export default actasList;
--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import FormInfo from '../src/formInfoView';
 
 export default class Home extends Component {
-
+    
     constructor(props) {
         super(props);
 
